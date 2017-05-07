5afac4ca8f1ac865afbc4a1dd222e5daa72f175a
Mike Plummer <mike-plummer@users.noreply.github.com>
2017-05-06 23:19:52 -0500

    Initial commit

---
+++ src/test/java/com/objectpartners/plummer/junit5/ApplicationTest.java
@@ -0,0 +1,8 @@
+package com.objectpartners.plummer.junit5;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public abstract class ApplicationTest {
+}
