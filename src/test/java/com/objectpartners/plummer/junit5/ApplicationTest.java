eb1205f5e5e06e749ca89657c17da305e76cd0b6
Mike Plummer <mike-plummer@users.noreply.github.com>
2017-05-14 14:27:03 -0500

    Scaffolding

--- src/test/java/com/objectpartners/plummer/junit5/ApplicationTest.java
+++
@@ -1,8 +0,0 @@
-package com.objectpartners.plummer.junit5;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-@ExtendWith(SpringExtension.class)
-public abstract class ApplicationTest {
-}
